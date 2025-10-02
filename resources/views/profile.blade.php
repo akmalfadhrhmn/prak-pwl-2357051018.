bcf361365e799a587c68c5bdf91601cd568308b3
akmal <akmalfadhurohman@gmail.com>
2025-10-02 19:54:11 +0700

    Selesai Langkah Praktikum Modul

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,15 @@
+<tr>
+    <td>Nama</td>
+    <td>:</td>
+    <td>{{ $nama }}</td>
+</tr>
+<tr>
+    <td>NPM</td>
+    <td>:</td>
+    <td>{{ $npm }}</td>
+</tr>
+<tr>
+    <td>Kelas</td>
+    <td>:</td>
+    <td>{{ $kelas }}</td>
+</tr>
